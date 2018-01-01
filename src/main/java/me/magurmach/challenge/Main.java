a0baf786ffaf5c419c24007a8fc147cfec309376
Shakib Ahmed <sheikahm@cisco.com>
2018-01-01 15:43:39 -0800

    Add implementation in Main to solve the problem

--- src/main/java/me/magurmach/challenge/Main.java
+++ src/main/java/me/magurmach/challenge/Main.java
@@ -1,19 +1,86 @@
 package me.magurmach.challenge;
 
+import me.magurmach.challenge.graph.Graph;
+import me.magurmach.challenge.graph.Node;
+import me.magurmach.challenge.graph.algorithm.MyDFS;
+import me.magurmach.challenge.util.IntPair;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by Shakib Ahmed on 1/1/18.
  */
 public class Main {
 
+    private static ArrayList<ArrayList<Node>> nodeGrid;
+    private static int directionX[] = {0, 0, 1, -1};
+    private static int directionY[] = {1, -1, 0, 0};
 
+    public static void processFile(String fileName) throws FileNotFoundException {
+        Scanner in;
+        in = new Scanner(new File(fileName));
+        int x, y;
+        x = in.nextInt();
+        y = in.nextInt();
 
-    public static void main(String[] args) {
-        String commandLineFileName = "test.txt";
+        nodeGrid = new ArrayList<>();
 
-        if (args[0].equals("-i")) {
-            commandLineFileName = args[1];
+        int i, j;
+        for (i = 0; i < x; i++) {
+            nodeGrid.add(new ArrayList<>());
+            for (j = 0; j < y; j++) {
+                int value = in.nextInt();
+                nodeGrid.get(i).add(new Node(i, j, value));
+            }
+        }
+        in.close();
+    }
+
+    private static boolean inRange(int x, int a, int b) {
+        return a <= x && x <= b;
+    }
+
+    private static Graph processGrid() {
+        Graph graph = new Graph();
+        int i, j, k;
+        for (i = 0; i < nodeGrid.size(); i++) {
+            for (j = 0; j < nodeGrid.get(i).size(); j++) {
+                for (k = 0; k < directionX.length; k++) {
+                    int nx = i + directionX[k];
+                    int ny = j + directionY[k];
+                    if (inRange(nx, 0, nodeGrid.size() -1) && inRange(ny, 0, nodeGrid.get(i).size())
+                            && nodeGrid.get(i).get(j).getValue() > nodeGrid.get(nx).get(ny).getValue()) {
+                        graph.addAdjacency(nodeGrid.get(i).get(j), nodeGrid.get(nx).get(ny));
+                    }
+                }
+            }
         }
 
-        System.out.println(commandLineFileName);
+        return graph;
+    }
+
+    public static void main(String[] args) {
+        String fileName = "test.txt";
+
+        if (args.length > 2 && args[0].equals("-i")) {
+            fileName = args[1];
+        }
+
+        try {
+            processFile(fileName);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found: " + e.getMessage());
+        }
+
+        Graph graph = processGrid();
+
+        MyDFS solver = new MyDFS(graph);
+
+        IntPair reult = solver.getMaximumDepthWithMaximumDrop();
+        System.out.printf("Maximum depth = %d, Maximum drop = %d\n",reult.getFirst(), reult.getSecond());
     }
 }
